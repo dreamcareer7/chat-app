5e952ddf44d822f99b3afd0ae523af1eb4107447
dreamcareer7 <dreamcareer070@gmail.com>
2022-12-27 17:35:54 +0000

    can send message in realtime

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ class MessagesController < ApplicationController
     # automatically generates association between message and logged-in user
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel", 
+        modified_message: message_render(message)
     end
   end
 
@@ -13,5 +14,9 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
