c2965215a01932e728bf592afccb3f7b3ada869c
dreamcareer7 <dreamcareer070@gmail.com>
2022-12-27 15:57:06 +0000

    can send a message in the chat

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
   resources :users
   resources :messages
 end
