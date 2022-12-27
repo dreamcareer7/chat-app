65567fb1e0006ed000a3079f5a67214ca7c42600
dreamcareer7 <dreamcareer070@gmail.com>
2022-12-27 15:13:39 +0000

    can log in and log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # ||= is memoization - will only perform if @current_user doesn't exist, to stop it querying the database repeatedly
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # !! makes it a boolean return - ie. if current_user is there, true, if not, false
+    !!current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
   resources :messages
 end
