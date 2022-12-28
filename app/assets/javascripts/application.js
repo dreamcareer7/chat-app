9ab2b51ab8d18fd362900182c0b8962140a9a3ef
dreamcareer7 <dreamcareer070@gmail.com>
2022-12-28 09:26:36 +0000

    send message by hitting enter (sends twice, needs fixing)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,16 +16,28 @@
 //= require semantic-ui
 //= require_tree .
 
-scroll_bottom = function() {
-  if ($('#messages').length > 0) {
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+scroll_bottom = function () {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
   }
-}
+};
+
+// submit message by hitting enter
+// get input id of #message_body from inspect element
+// keycode 13 means the enter key
+submit_message = function () {
+  $("#message_body").on("keydown", function (e) {
+    if (e.keyCode === 13) {
+      $("button").click();
+    }
+  });
+};
 
 $(document).on("turbolinks:load", function () {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function () {
     $(this).closest(".message").transition("fade");
   });
+  submit_message();
   scroll_bottom();
 });
